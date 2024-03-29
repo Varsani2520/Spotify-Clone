2a8c7dbd8eb7b5419d2c983ee102317c93a2e3a9
Varsani2520 <varsaniranjani2520@gmail.com>
2024-03-29 10:23:07 +0530

    Initial commit from Create Next App

---
+++ app/first.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+const first = () => {
+    useEffect(()=>{
+        fetch('')
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default first
