e76f0641e39189bd2109010ec35a70501f854a61
Varsani2520 <varsaniranjani2520@gmail.com>
2024-04-05 20:33:11 +0530

    done

--- app/playlists/[playlist_id]/page.jsx
+++ app/playlists/[playlist_id]/page.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 import style from '../../style.css'
 import DetailPlaylistFirst from '@/app/components/DetailPlaylistFirst';
 import DetailPlalistAlbum from '@/app/components/DetailPlalistAlbum';
+import CardPlaylistCenter from '@/app/components/CardPlaylistCenter';
 const Page = () => {
 
 
     return (
         <>
             <DetailPlaylistFirst />
+            <CardPlaylistCenter />
             <DetailPlalistAlbum />
         </>
     );
